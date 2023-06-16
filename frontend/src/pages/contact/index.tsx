7d7dbb10a5629d76319a7d76cb4e653cd803b19d
Grzegorz Grzegrzółka <ggrzegrzolka11@gmail.com>
2023-06-16 12:41:21 +0200

    add pages for every section

---
+++ frontend/src/pages/contact/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '~/components/layout'
+import Contact from '~/sections/contact'
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <Contact />
+    </Layout>
+  )
+}
+
+export default ContactPage
---
+++ frontend/src/pages/prices/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '~/components/layout'
+import Prices from '~/sections/prices'
+
+const PricesPage = () => {
+  return (
+    <Layout>
+      <Prices />
+    </Layout>
+  )
+}
+
+export default PricesPage
