7151f24ffdd0076b72b8a5f3b25b7bacb4a9d511
Grzegorz Grzegrzółka <ggrzegrzolka11@gmail.com>
2023-06-16 13:14:24 +0200

    footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer className="w-screen py-6 text-center">
+      Copyright &copy; 2023 Ljuskristallen. All rights reserved.
+    </footer>
+  )
 }
 
 export default Footer
