ffc62804cf54f3758e87efb15f0e20b9ff6eba61
Grzegorz Grzegrzółka <ggrzegrzolka11@gmail.com>
2023-06-19 12:48:08 +0200

    prices section

---
+++ frontend/src/constants/plans.ts
@@ -0,0 +1,49 @@
+const Plans = [
+  {
+    title: 'Helande',
+    description:
+      '1,5h Session med Maria där ni går på djupet och du får hjälp med dina inre problem du bär på och rensar dig på djupet.',
+    price: '800kr/1.5h'
+  },
+  {
+    title: 'Mindfulness',
+    description:
+      '1h Session med Maria där ni lär er total avslappning och djupandning.',
+    price: '600kr/1h'
+  },
+  {
+    title: 'Yoga',
+    description: '1h Session med Jin Yoga.',
+    price: '400kr/1h'
+  },
+  {
+    title: 'Meditation',
+    description: '30 minuter djup avslappning.',
+    price: '250kr/30min'
+  },
+  {
+    title: 'Jacuzzi + Bastu',
+    description:
+      'Basta i vår sköna bastu och slappna sedan av i den underbara varma bubbelpoolen.',
+    price: '600kr/1h'
+  },
+  {
+    title: 'Lunch',
+    description: 'Rova vår delikata räkmacka inkl. sallad och dryck.',
+    price: '200kr'
+  },
+  {
+    title: 'Övernattning',
+    description:
+      'Sov över en natt i vårt sköna avkopplande attefallshus i underbar trädgårdsmiljö.',
+    price: '800kr'
+  },
+  {
+    title: 'Kaffe eller Te',
+    description:
+      'Ta en uppiggande kopp Latte, Cappucino eller varför inte en läkande kopp rött/svart/grönt/ört te.',
+    price: '39kr'
+  }
+]
+
+export default Plans
--- frontend/src/sections/prices.tsx
+++ frontend/src/sections/prices.tsx
@@ -1,5 +1,24 @@
+import Plans from '~/constants/plans'
+
 const Prices = () => {
-  return <div>Prices</div>
+  return (
+    <div className="mx-auto my-14 mt-28 flex max-w-screen-xl flex-col items-center justify-center gap-12 pt-16">
+      <span className="text-3xl">Våra Priser</span>
+      <div className="grid grid-cols-4 grid-rows-2 gap-4">
+        {Plans.map((plan, index) => (
+          <div key={`plan-${index}`}>
+            <div className="flex h-full flex-col items-center overflow-auto rounded-lg bg-[purple] p-4 text-white">
+              <span className="text-center text-xl">{plan.title}</span>
+              <span className="text-center text-2xl font-bold">
+                {plan.price}
+              </span>
+              <span className="text-center">{plan.description}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Prices
