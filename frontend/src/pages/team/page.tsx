92488dc327af5a865feb64a92e67c36e34412eab
Grzegorz Grzegrzółka <ggrzegrzolka11@gmail.com>
2023-06-21 14:50:57 +0200

    team page

---
+++ frontend/src/pages/team/page.tsx
@@ -0,0 +1,14 @@
+import Layout from '~/components/layout'
+import Info from '~/sections/info'
+import Team from '~/sections/team'
+
+const TeamPage = () => {
+  return (
+    <Layout>
+      <Team />
+      <Info />
+    </Layout>
+  )
+}
+
+export default TeamPage
