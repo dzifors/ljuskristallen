82a878f6e59cf6c941bf535c11ddb6c7fafc01e4
Grzegorz Grzegrzółka <ggrzegrzolka11@gmail.com>
2023-06-22 14:43:59 +0200

    admin page under construction message

---
+++ frontend/src/pages/admin/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '~/components/layout'
+
+const AdminPage = () => {
+  return (
+    <Layout>
+      <section>Under construction</section>
+    </Layout>
+  )
+}
+
+export default AdminPage
